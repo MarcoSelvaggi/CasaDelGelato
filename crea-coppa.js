28dfb8741e093afd51fd01aea936c1a44748f581
MarcoSelvaggi <129788346+MarcoSelvaggi@users.noreply.github.com>
2026-01-07 11:27:37 +0100

    Add files via upload

--- crea-coppa.js
+++ crea-coppa.js
@@ -2256,7 +2256,7 @@ if (coppaSelezionata === "PICCOLA") {
 
 <div class="arrow"
      id="topping-arrow"
-     style="top:-50px; left:50%; transform:translateX(-50%);">
+     style="top:-45px; left:50%; transform:translateX(-50%);">
 
     <div class="arrow-text top topping-text" id="topping-text">
       Topping
